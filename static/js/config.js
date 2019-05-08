5bba214eb8a6bb86b9a14bc08c7cc1a41370cf55
Nandu78 <44281630+Nandu78@users.noreply.github.com>
2019-05-08 15:11:06 -0500

    Add files via upload

---
+++ static/js/config.js
@@ -0,0 +1,4 @@
+// API key
+// const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+
+const API_KEY = "pk.eyJ1IjoibmFuZHViaGFpIiwiYSI6ImNqdmZua2FvNDJ3YXM0ZW9qOGJpbG5jcTgifQ.f-ce--Ud0JG0u2QS6RnrVQ";
